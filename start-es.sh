15e92f78f281c624c4b461839fe7f681387707f2
Max Dudek <maxdudek@gmail.com>
2020-05-19 17:18:31 -0400

    Updated to 7.7

--- start-es.sh
+++ start-es.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -e
-screen -d -m -S es bash -c 'ES_PATH_CONF=~/elasticsearch-test/config/ ~/es/elasticsearch-7.2.0/bin/elasticsearch'
+screen -d -m -S es bash -c 'ES_PATH_CONF=~/elasticsearch-test/config/ ~/es/elasticsearch-7.7.0/bin/elasticsearch'
